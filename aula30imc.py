c329762eefa820d13ce4501571910009e90222c7
Edgar Everton <edgareverton@outlook.com>
2025-08-31 16:50:08 -0300

    formatF

--- aula30imc.py
+++ aula30imc.py
@@ -2,4 +2,5 @@ nome = 'Edgar Everton'
 peso = 78
 altura = 1.64
 imc = peso / altura ** 2
-print('O IMC de ', nome, ' é ', imc )
+linha1 = f'O IMC de {nome} é {imc:.2f} é seu peso é {peso} e a altura é {altura} '
+print(linha1 )
