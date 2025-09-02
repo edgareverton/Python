d7282e0b931ad7ec00b8fc37d62e5d8d12d5be66
Edgar Everton <edgareverton@outlook.com>
2025-09-01 22:40:16 -0300

    condicionais

---
+++ condicionais.py
@@ -0,0 +1,8 @@
+entrada = input('Digite "entrar" ou "sair": ')
+
+if entrada == "entrar":
+    print("você entrou no sitema")
+elif entrada == "sair":
+    print('Você saiu do sistema')
+else: 
+    print('Você não digitou "entrar" e nem "sair"')
