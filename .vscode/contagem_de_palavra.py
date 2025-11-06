144243239454a0be64975931ce4e8bd0c3d26180
Edgar Everton <edgareverton@outlook.com>
2025-11-06 12:20:57 -0300

    iteracao_strings

--- .vscode/contagem_de_palavra.py
+++ .vscode/contagem_de_palavra.py
@@ -3,8 +3,25 @@ frase = ' Esta é uma frase somente para ' \
 ' nada de mais né kkkkk'
 
 i = 0 
+qtd_apareceu_mais_vezes = 0
+letra_q_apareceu_mais_vezes = ''
 while i < len(frase):
     letra_atual = frase[i]
-    quantas_vezes_letra_apareceu = frase.count(letra_atual)
-    print(letra_atual, quantas_vezes_letra_apareceu)
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+        letra_q_apareceu_mais_vezes = letra_atual
+   
     i += 1
+
+print   (
+          'A letra que apareeu mais vezes foi '
+          f'"{letra_q_apareceu_mais_vezes }" que apareceu '
+          f'{qtd_apareceu_mais_vezes} x'
+         )
