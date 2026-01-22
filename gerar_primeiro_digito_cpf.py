0950c12b7cfae906372db765111a28a335df608d
Edgar Everton <edgareverton@outlook.com>
2026-01-22 00:09:34 -0300

    ...

--- gerar_primeiro_digito_cpf.py
+++ gerar_primeiro_digito_cpf.py
@@ -23,12 +23,19 @@ contrário disso:
 
 O primeiro dígito do CPF é 7
 """
-cpf =input('Digite seu CPF: ')
+cpf = '74682489070'
 nove_digito = cpf[:9]
-contador_regressivo = 10
+contador_regressivo_1 = 10
 
-resultado = 0
-for digito in nove_digito:
-    resultado += int(digito) * contador_regressivo
-    contador_regressivo -=1
-print(resultado)
+resultado_digito_1 = 0
+for digito_1 in nove_digito:
+    resultado_digito_1 += int(digito_1) * contador_regressivo_1
+    contador_regressivo_1 -=1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+print(digito_1)
+
+Dez_digito = nove_digito + str(digito_1)
+contador_regressivo_2 = 11
+
+print(Dez_digito)
