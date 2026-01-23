ff3c5f18347291fda8cd85ee5d86685eb7411b36
Edgar Everton <edgareverton@outlook.com>
2026-01-22 21:18:23 -0300

    return

---
+++ return.py
@@ -0,0 +1,9 @@
+def soma( x, y):
+     if x > 10:
+          return print('X é maior que 10')
+     return x + y
+soma1 = soma(2,2)
+soma2 = soma(3,3)
+print(soma(11,10))
+print(soma1)
+print(soma2)
