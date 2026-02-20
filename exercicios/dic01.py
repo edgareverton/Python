eae2a74365d051a52537a746a2d3264d173b591f
Edgar Everton <edgareverton@outlook.com>
2026-02-20 16:35:33 -0300

    dic01

---
+++ exercicios/dic01.py
@@ -0,0 +1,21 @@
+p1 = {
+    'nome': 'Edgar',
+    'endereco': 'Av. Ayeton S.'
+}
+# nome = p1.pop('nome')
+# print(nome)
+# print(p1['nome'])
+# print(p1)
+
+# ultima_chave = p1.popitem()
+
+# print(p1)
+# print(ultima_chave)
+
+# p1.update({
+#     'nome': 'Everton',
+#     'idade': 29
+# })
+
+p1.update(nome='Everton', idade=0)
+print(p1)
