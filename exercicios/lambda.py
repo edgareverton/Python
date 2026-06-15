66060eb1f059886b97dfe9ab561d68c87a6feb6e
Edgar Everton <edgareverton@outlook.com>
2026-06-15 00:45:26 -0300

    lambda v1

---
+++ exercicios/lambda.py
@@ -0,0 +1,13 @@
+lista = [ 
+    {'nome': 'Edgar', 'sobrenome': 'Everton'},
+    {'nome': 'Peter', 'sobrenome': 'Hurs'},
+    {'nome': 'Julieta', 'sobrenome': 'Miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Aparecida'},
+]
+
+def ordena(item):
+    print(item)
+    return item
+
+lista.sort()
+print(lista)
